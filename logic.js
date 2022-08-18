61d6526ff0a798f44e8efe6c470241e26ad76930
HaydenHildreth <36833433+HaydenHildreth@users.noreply.github.com>
2022-08-18 18:01:09 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 function generateNumber() {
-    let value = Math.floor(Math.random() * 26) + 1;
+    let value = Math.floor(Math.random() * 101) + 1;
     console.log(value)
     return value;
 }
@@ -8,6 +8,19 @@ function select() {
     var guess = document.querySelector('#flag');
     var option = guess.value;
     console.log(option)
+}
+
+function check(option) {
+    if (option = value)
+    {
+        console.log('Right answer');
+        //generateNumber(); new number
+    }
+    else 
+    {
+        console.log('Incorrect');
+        //endGameFoo(); make foo
+    }
 }
 
 generateNumber();
